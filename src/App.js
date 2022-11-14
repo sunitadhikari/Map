69e009612b914e72f534d9f61f54a42f0eb680ac
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-14 21:08:31 +0545

    message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react'
+import Users from './Users'
+function App() {
+  return (
+    <div>
+    <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Users() {
+    const data=[
+        {
+            id:1,name:"john doe"
+        },
+        {
+            id:1,name:"john doe"
+        },
+        {
+            id:1,name:"john doe"
+        },
+    ]
+    return (
+    <div>
+      
+    </div>
+  )
+}
+export default Users
