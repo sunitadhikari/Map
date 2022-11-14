99d687ff7739e9536e7a6a6f9c5c4c92d2a327d7
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-14 21:18:57 +0545

    message

--- src/Users.js
+++ src/Users.js
@@ -3,18 +3,24 @@ import React from 'react'
 function Users() {
     const data=[
         {
-            id:1,name:"john doe"
+            id:1,name:"Sunita Adhikari"
         },
         {
-            id:1,name:"john doe"
+            id:2,name:"Sangita Adhikari"
         },
         {
-            id:1,name:"john doe"
+            id:3,name:"Sita Adhikari"
         },
     ]
     return (
     <div>
-      
+      {
+        data.map((user)=>(
+            <div>
+               {user.name}
+            </div>
+        ))
+      }
     </div>
   )
 }
